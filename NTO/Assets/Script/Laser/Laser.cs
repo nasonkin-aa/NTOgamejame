bcd414154b3456d4fb85bcd29632fcd8eea6c8d7
nasonkin-aa <game-akaynt@mail.ru>
2022-02-12 12:53:47 +1000

    Add Laser

--- NTO/Assets/Script/Laser/Laser.cs
+++ NTO/Assets/Script/Laser/Laser.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _defDistanceRay = 100;
+    public Transform laserFirePoint;
+    public LineRenderer m_lineRenderer;
+    Transform m_transform;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        m_transform = GetComponent<Transform>();
+    }
+    void ShootLaser()
+    {
+        if (Physics2D.Raycast(m_transform.position, transform.right))
+        {
+            RaycastHit2D _hit = Physics2D.Raycast(laserFirePoint.position, transform.right);
+            Draw2DRay(laserFirePoint.position, _hit.point);
+        }
+        else
+        {
+            Draw2DRay(laserFirePoint.position, laserFirePoint.transform.right * _defDistanceRay);
+
+        }
+    }
+    void Draw2DRay(Vector2 startPos, Vector2 endPos)
+    {
+        m_lineRenderer.SetPosition(0, startPos);
+        m_lineRenderer.SetPosition(1, endPos);
+
     }
 }
